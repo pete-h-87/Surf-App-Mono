44653e3d1e893df9a38f2b9918c12c39d470da44
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2025-01-01 23:38:42 +0100

    adding postinstall on backend package.json

--- backend/routes/screenshotRoutes.js
+++ backend/routes/screenshotRoutes.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      executablePath: puppeteer.executablePath(), // Use Puppeteer's default executable path
+      executablePath: process.env.CHROME_PATH || puppeteer.executablePath(), // Use Puppeteer's default executable path if CHROME_PATH is not set
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
