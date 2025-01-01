a739645738bf0b7232a5de2f1a4513dd1e44b844
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2025-01-01 20:46:42 +0100

    added dotenv to dabase.js to grab the env variables from render

--- backend/model/forecast.sql
+++ backend/model/forecast.sql
@@ -28,3 +28,10 @@ CREATE TABLE snapshot(
 
 DELETE FROM forecast;
 
+
+
+-- for gitBash:
+
+CREATE TABLE forecast(journal_id SERIAL PRIMARY KEY, date_recorded DATE NOT NULL, session_time VARCHAR NOT NULL, wind_speed DECIMAL(5, 2) NOT NULL, wind_direction DECIMAL(5, 2) NOT NULL, wave_height DECIMAL(5, 2) NOT NULL, wave_period DECIMAL(5, 2) NOT NULL, wave_direction DECIMAL(5,2) NOT NULL, temperature DECIMAL(5, 2) NOT NULL);
+
+CREATE TABLE journal(journal_id SERIAL PRIMARY KEY, forecast_id INT REFERENCES forecast(journal_id), prediction VARCHAR, report VARCHAR);
