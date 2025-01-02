912695368700ec8172edaceb8679c541594b25e8
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2025-01-02 20:34:01 +0100

    attemp 2

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,12 +1,14 @@
-# !/usr/bin/env bash
+#!/usr/bin/env bash
 # Exit on error
-
 set -o errexit
+
 npm install
-
 # npm run build # Uncomment if required
-# Store/pull Puppeteer cache with build cache
 
+# Ensure the Puppeteer cache directory exists
+mkdir -p $PUPPETEER_CACHE_DIR
+
+# Store/pull Puppeteer cache with build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
