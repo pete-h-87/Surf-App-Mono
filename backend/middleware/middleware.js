923064fe697c0d7b25ad26fe13a1957d0ee4ea90
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2025-06-11 20:36:35 +0200

    more

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,10 +1,5 @@
 function checkSession(req, res, next) {
-  console.log(
-    "the REQ in checkSession:",
-    req.session,
-    req.session.passport,
-    req.session.passport.user
-  );
+  console.log("the REQ pass port in checkSession:", req.session.passport);
   if (!req.session || !req.session.passport || !req.session.passport.user) {
     return res.status(401).json({ message: "Session expired" });
   }
