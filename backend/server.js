9a559e5c3825b2758e78ca99bdf32685873633cd
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2025-12-22 20:19:04 +0100

    adding a filter boolean

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ const port = process.env.PORT || 10000;
 app.set("trust proxy", 1);
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_WWW],
+  origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_WWW].filter(
+    Boolean
+  ),
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
