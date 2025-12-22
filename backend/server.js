02416bb933d6a14dc90af09ad00d2f8614379bb0
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2025-12-22 19:29:53 +0100

    moving dotenv require to very first line

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const passport = require("passport");
 // const initializePassport = require("./local-strategy");
 const app = express();
