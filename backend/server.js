6f983ce52cea5a34166c8175769f29d509bd7074
Peter Hufton <152397945+petey-boi@users.noreply.github.com>
2025-12-22 17:05:10 +0100

    commenting out domain

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(
     saveUninitialized: false,
     rolling: true,
     cookie: {
-      domain: ".niche-surf.com",
+      // domain: ".niche-surf.com",
       maxAge: 30 * 60 * 1000,
       sameSite: "lax",
       secure: false,
